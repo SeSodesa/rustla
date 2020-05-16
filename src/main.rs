b63f4aeeeb27b7f4a2d3663407c3ce0528af6933
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-16 09:17:17 +0300

    Added starting comments and a new usage function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+/// This is ruSTLa
+/// 
+/// author: Santtu Söderholm
+///  email: santtu.soderholm@tuni.fi
+
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const AUTHOR_NAME: &'static str = env!("AUTHOR_NAME");
 const AUTHOR_EMAIL: &'static str = env!("AUTHOR_EMAIL");
@@ -5,10 +10,22 @@ const AUTHOR_EMAIL: &'static str = env!("AUTHOR_EMAIL");
 fn main() {
     
   copyright();
+  usage();
 
 }
 
 fn copyright() {
   println!("\nThis is ruSTLa, version {}\n", VERSION);
   println!("©{}, {}\n", AUTHOR_NAME, AUTHOR_EMAIL);
+}
+
+fn usage() {
+  println!("Instructions");
+  println!("============");
+  println!("In order to transpile a set of documents");
+  println!("point ruSTLa to a directory that contains");
+  println!("and index.rst file with");
+  println!("\n  $ rustla <dir>\n");
+  println!("A single file cab be transpiled with");
+  println!("  $ rustla <file>");
 }
