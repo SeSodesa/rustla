472e9731c1b2d7f04e771135291506338be4dfe7
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-16 06:41:56 +0300

    Added a link to build script documentation

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
+/// This build script is compiled before
+/// the actual main program and its dependencies.
+/// 
+/// Text sent to standard output by this script
+/// is handled in specific ways by Cargo,
+/// depending on how each output line starts.
+/// 
+/// For full details, see
+/// https://doc.rust-lang.org/cargo/reference/build-scripts.html
+
 fn main() {
   println!("cargo:rustc-env=AUTHOR_NAME=Santtu Söderholm");
   println!("cargo:rustc-env=AUTHOR_EMAIL=santtu.soderholm@tuni.fi");
