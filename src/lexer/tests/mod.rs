55ff0f0541bec8492e1629534337d130d805c6d8
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-17 21:36:57 +0300

    Added functions for lexing (to be implemented), and renamed lexeme_current -> lookahead

--- src/lexer/tests/mod.rs
+++ src/lexer/tests/mod.rs
@@ -10,7 +10,43 @@ fn new() {
   let lex = Lexer::new(ls);
   assert_eq!(lex.tokens, Vec::new());
   assert_eq!(lex.lexeme_start, 0);
-  assert_eq!(lex.lexeme_current, 0);
+  assert_eq!(lex.lookahead, 0);
   assert_eq!(lex.row, 0);
   assert_eq!(lex.col, 0);
+}
+
+
+#[test]
+/// Tests the scanning of the entire source
+fn scan_tokens() {
+  panic!();
+}
+
+
+#[test]
+/// Tests the scanning of a single token
+fn scan_token() {
+  panic!();
+}
+
+
+#[test]
+/// Test the advancement of the
+/// "lexing buffer"
+fn advance() {
+  panic!();
+}
+
+
+#[test]
+/// Tests the addition of a token to
+/// the Lexer field `tokens`
+fn add_token() {
+  panic!();
+}
+
+#[test]
+/// A test for finding the EOF
+fn is_at_eof() {
+  panic!();
 }
