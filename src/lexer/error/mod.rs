b7a914eeb1ff5ab440da3c356312516f7d0fb069
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-19 10:49:02 +0300

    Added a new error crate::lexer::error::LexError

--- src/lexer/error/mod.rs
+++ src/lexer/error/mod.rs
@@ -12,6 +12,11 @@ pub struct TokenizeError <'a> {
   col: &'a usize,
 }
 
+pub struct LexError <'a> {
+  row: &'a usize,
+  col: &'a usize,
+}
+
 impl<'a> TokenizeError<'a>  {
   pub fn new(row: &'a usize, col: &'a usize) -> TokenizeError<'a> {
     TokenizeError{
@@ -25,5 +30,23 @@ impl<'a> fmt::Display for TokenizeError <'a> {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     write!(f, "TokenizeError: \
     no matching lexeme for <row, col> = <{}, {}>", self.row, self.col)
+  }
+}
+
+
+impl<'a> LexError<'a>  {
+  pub fn new(row: &'a usize, col: &'a usize) -> LexError<'a> {
+    LexError{
+      row: row,
+      col: col
+    }
+  }
+}
+
+
+impl<'a> fmt::Display for LexError <'a> {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "LexError: \
+    something went wrong because of <row, col> = <{}, {}>", self.row, self.col)
   }
 }
