3a80efee57663fdcd5ed24bc9bd0e54ce708d0eb
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-21 08:46:39 +0300

    Added files related to the crate::lexer::regex submodule

---
+++ src/lexer/regex/mod.rs
@@ -0,0 +1,7 @@
+/// This module contains the regexes used
+/// by the Lexer, wrapped in a function
+
+mod test;
+
+use regex::Regex;
+use lazy_static::lazy_static;
---
+++ src/lexer/regex/test/mod.rs
@@ -0,0 +1,5 @@
+/// This is a test module for the Lexer
+/// regexes.
+
+use super::*;
+
