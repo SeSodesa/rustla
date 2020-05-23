25d7c0d9aac6b368ceb2d9bf736e20885d878723
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 08:07:05 +0300

    Simplified the literal block regex significantly

--- src/lexer/token_mappings/blocks/mod.rs
+++ src/lexer/token_mappings/blocks/mod.rs
@@ -6,7 +6,7 @@ mod test;
 use crate::lexer::token::TokenType;
 
 static BLOCK_RE_MAP: &[(TokenType, &'static str)] = &[
-  (TokenType::LiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)([ \t]+)(.*)(\n)((?:(?:[ \t]+.*)?\n)+)"),
+  (TokenType::LiteralBlock, r"(?m)::\s*\n[ \t]+.*\n(?:(?:[ \t]+.*)?\n)+"),
   (TokenType::PerLineLiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)(>+ .*\n|>+[ \t]*\n)+\s*"),
   (TokenType::LineBlock, r"(?m)^\s*(?:\|.*\n|\|[ \t]*)+\s*"),
   (TokenType::Paragraph, r"(?m)^\s*(?:^.+\n)+\s+"),
