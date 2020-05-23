808b5af874e83958ce772916a0ebb98eb8d5bd64
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 08:21:13 +0300

    Simplified literal block regex and added a newline safeguard at the end

--- src/lexer/token_mappings/blocks/mod.rs
+++ src/lexer/token_mappings/blocks/mod.rs
@@ -7,7 +7,7 @@ use crate::lexer::token::TokenType;
 
 static BLOCK_RE_MAP: &[(TokenType, &'static str)] = &[
   (TokenType::LiteralBlock, r"(?m)::\s*\n[ \t]+.*\n(?:(?:[ \t]+.*)?\n)+"),
-  (TokenType::PerLineLiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)(>+ .*\n|>+[ \t]*\n)+\s*"),
+  (TokenType::PerLineLiteralBlock, r"(?m)::\s*\n(>+ .+\n|>+[ \t]*\n)+\s*\n"),
   (TokenType::LineBlock, r"(?m)^\s*(?:\|.*\n|\|[ \t]*)+\s*"),
   (TokenType::Paragraph, r"(?m)^\s*(?:^.+\n)+\s+"),
 ];
