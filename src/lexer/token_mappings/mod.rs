043bd4fee6d2d55c7c9ef8a4502d087fd6545812
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 14:17:08 +0300

    lexer::token_mappings now uses the project stucture established in @2cb068a

--- src/lexer/token_mappings/mod.rs
+++ src/lexer/token_mappings/mod.rs
@@ -1,17 +1,29 @@
 /// This module contains the regexes used
 /// by the Lexer.
 
-pub mod titles;
-pub mod lists;
-pub mod blocks;
+// pub mod titles;
+// pub mod lists;
+// pub mod blocks;
+mod body;
 
+use std::collections::HashMap;
+use crate::lexer::state::State;
 use crate::lexer::token::TokenType;
+
+/// ### delta
+/// The state transition function of the lexer.
+/// Returns a list of Token--action--next_state
+/// tuples to the lexer, the the lexer uses to
+/// process its source. 
+pub fn delta() {
+
+}
 
 /// ### val_from_key
 /// Searches through a list of TokenType--regex pairs
 /// for a mathing tokentype
-pub fn val_from_key(search_key: &TokenType, map: &[(TokenType, &'static str)]) -> Option<String> {
-  for (_, val) in map.iter().filter(|&(map_key, _)| map_key == search_key) { 
+pub fn val_from_key(search_key: &TokenType, map: &'static [(TokenType, &'static str, State)]) -> Option<String> {
+  for (_, val, _) in map.iter().filter(|&(map_key, _, _)| map_key == search_key) { 
     return Some(val.to_string());
   }
   None
