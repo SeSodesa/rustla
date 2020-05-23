25f96f3ebf74fc61d722c5a306df211a21b3097a
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 10:48:36 +0300

    Added a new lexer::state submodule with a State enum

---
+++ src/lexer/state/mod.rs
@@ -0,0 +1,16 @@
+/// This submodule contains the
+/// set of possible states the lexer
+/// could be in
+
+
+#[derive(Debug)]
+/// ### State
+/// This enum lists the possible states the
+/// lexer could be in. reStructuredText
+/// contains only body and inline elements,
+/// so we stick with those.
+pub enum State {
+  Body,
+  Inline
+}
+
