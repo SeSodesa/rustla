6e0e13b07d8d13bb3703046ae68bd1d6dbbf4d09
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 11:30:49 +0300

    Added some documentation to lexer::state

--- src/lexer/state/mod.rs
+++ src/lexer/state/mod.rs
@@ -9,8 +9,20 @@
 /// lexer could be in. reStructuredText
 /// contains only body and inline elements,
 /// so we stick with those.
+/// 
+/// The input in lexed differently in different
+/// states. While in Body, new body elements
+/// are scanned. While in inline, inline
+/// elements are scanned.
+/// 
+/// If a body element that might contain
+/// inline elements is encountered,
+/// a transition to Inline state is made.
+/// Once the end of the body element is reached,
+/// a switch to Body is made.
+/// EOF ends lexing.
 pub enum State {
   Body,
-  Inline
+  Inline,
 }
 
