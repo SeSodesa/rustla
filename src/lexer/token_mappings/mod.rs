e253922bfdb280af2fd460a5e55e6a6a72543b39
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 07:28:32 +0300

    Removed an unnecessary lifetime specifier

--- src/lexer/token_mappings/mod.rs
+++ src/lexer/token_mappings/mod.rs
@@ -8,8 +8,9 @@ pub mod blocks;
 use crate::lexer::token::TokenType;
 
 /// ### val_from_key
-/// Searches through a 
-pub fn val_from_key<'t>(search_key: &TokenType, map: &[(TokenType, &'static str)]) -> Option<String> {
+/// Searches through a list of TokenType--regex pairs
+/// for a mathing tokentype
+pub fn val_from_key(search_key: &TokenType, map: &[(TokenType, &'static str)]) -> Option<String> {
   for (_, val) in map.iter().filter(|&(map_key, _)| map_key == search_key) { 
     return Some(val.to_string());
   }
