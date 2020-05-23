9b146d41636c415090422c78f125d5db2eb494ae
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 07:44:29 +0300

    Line blocks now consume extra empty lines after them

--- src/lexer/token_mappings/blocks/mod.rs
+++ src/lexer/token_mappings/blocks/mod.rs
@@ -8,6 +8,6 @@ use crate::lexer::token::TokenType;
 static BLOCK_RE_MAP: &[(TokenType, &'static str)] = &[
   (TokenType::LiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)([ \t]+)(.*)(\n)((?:(?:[ \t]+.*)?\n)+)"),
   (TokenType::PerLineLiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)(> .+\n)*[ \t\n]*"),
-  (TokenType::LineBlock, r"(?m)^\s*(?:\| .+\n)+"),
+  (TokenType::LineBlock, r"(?m)^\s*(?:\|.*\n|\|[ \t]*)+\s*"),
   (TokenType::Paragraph, r"(?m)^\s*(?:^.+\n)+\s+"),
 ];
