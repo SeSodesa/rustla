01b15bf06185f42959fd239a0d01b4c1c60b4e14
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 07:49:35 +0300

    Per line literal blocks are now a little more robust
    Multiple > symbols and empty lines starting with > are supported

--- src/lexer/token_mappings/blocks/mod.rs
+++ src/lexer/token_mappings/blocks/mod.rs
@@ -7,7 +7,7 @@ use crate::lexer::token::TokenType;
 
 static BLOCK_RE_MAP: &[(TokenType, &'static str)] = &[
   (TokenType::LiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)([ \t]+)(.*)(\n)((?:(?:[ \t]+.*)?\n)+)"),
-  (TokenType::PerLineLiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)(> .+\n)*[ \t\n]*"),
+  (TokenType::PerLineLiteralBlock, r"(?m)(::)[ \t]*(\n[ \t]*\n)(>+ .*\n|>+[ \t]*\n)+\s*"),
   (TokenType::LineBlock, r"(?m)^\s*(?:\|.*\n|\|[ \t]*)+\s*"),
   (TokenType::Paragraph, r"(?m)^\s*(?:^.+\n)+\s+"),
 ];
