61eac5c556b71802af6fb3e266e5b9fb86ac9549
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-05-23 14:24:24 +0300

    Removed unnecessary lifetime specifier

--- src/lexer/token_mappings/mod.rs
+++ src/lexer/token_mappings/mod.rs
@@ -19,7 +19,7 @@ pub fn delta() {
 /// ### val_from_key
 /// Searches through a list of TokenType--regex pairs
 /// for a mathing tokentype
-pub fn val_from_key(search_key: &TokenType, map: &'static [(TokenType, &'static str, State)]) -> Option<String> {
+pub fn val_from_key(search_key: &TokenType, map: &[(TokenType, &'static str, State)]) -> Option<String> {
   for (_, val, _) in map.iter().filter(|&(map_key, _, _)| map_key == search_key) { 
     return Some(val.to_string());
   }
