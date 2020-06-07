181c125da2b77a477ce4a5eb96464662f34c5607
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-06-07 15:40:50 +0300

    Updated the list of StateMachine states
    This was done based on the required states listed at https://sourceforge.net/p/docutils/code/HEAD/tree/trunk/docutils/docutils/parsers/rst/states.py#l202

--- src/parser/state_machine/states.rs
+++ src/parser/state_machine/states.rs
@@ -1,23 +1,30 @@
 /// This submodule contains the states and related transitions of
 /// the parser state machine.
 
+#[derive(Hash, Debug, PartialEq, Eq)]
+/// ### State
+/// An enumeration of the possible states of `StateMachine`.
+/// The indentations present here are an attempt to reflect
+/// the class hierarchy found in the docutils rST parser States module:
+///   https://sourceforge.net/p/docutils/code/HEAD/tree/trunk/docutils/docutils/parsers/rst/states.py#l202
 pub enum State {
-  Root,
-  Structural,
-    SectionLine,
-    
-  Body,
+  General,
+    Body,
+      SpecializedBody,
+        BulletList,
+        DefinitionList,
+        EnumeratedList,
+        FieldList,
+          ExtensionOptions,
+        OptionList,
+        RFC2822List,
+        LineBlock,
+        Explicit,
+      SubstitutionDefinition,
     Text,
-    BulletList,
-    EnumeratedList,
-
-    FieldList,
-      Field,
-    DefinitionList,
-      Definition,
-    OptionList,
-      Option,
-
-  Inline,
+      SpecializedText,
+        Definition,
+        Line,
+    QuotedLiteralBlock,
 
 }
