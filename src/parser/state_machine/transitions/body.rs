c425b400acd5c39031881ed0e813e0a27a9f6722
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-06-07 16:08:51 +0300

    Removed many many lines from parser as aunnecessary (rework) and renamed lexing methods to parsing methods

--- src/parser/state_machine/transitions/body.rs
+++ src/parser/state_machine/transitions/body.rs
@@ -2,217 +2,97 @@ use super::*;
 
 
 
-pub fn tokenize_blank_lines (lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}", tt);
-
-  let m = cs.get(0).unwrap();
-
-  lex.set_lexeme_limits(&m);
-
-  lex.tokens.push(
-    Token::new(
-      tt,
-      String::from("\n\n"),
-      m.start() + lex.pos.pos,
-      m.end() + lex.pos.pos,
-    )
-  );
-
-  // lex.update_pos();
+pub fn parse_blank_lines (lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
 }
 
 
-/// ### tokenize_section_title
+/// ### parse_section_title
 /// Creates the tokens related to overlined titles
-pub fn tokenize_section_title (lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}\n", tt);
-
-  let ws = cs.get(1).unwrap();
-
-  lex.set_lexeme_limits(&ws);
-
-  lex.tokens.push(
-    Token::new(
-      TokenType::BlankLines,
-      String::from("\n\n"),
-      ws.start() + lex.pos.pos,
-      ws.end() + lex.pos.pos,
-    )
-  );
-
-  //lex.update_pos();
-
-  let text = cs.get(3).unwrap();
-  let title = cs.get(2).unwrap();
-
-  lex.set_lexeme_limits(&title);
-
-  lex.tokens.push(
-    Token::new(
-      tt,
-      String::from(text.as_str()),
-      title.start() + lex.pos.pos,
-      title.end() + lex.pos.pos,
-    )
-  );
-
-  // lex.update_pos();
+pub fn parse_section_title (lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
 }
 
 
-/// ### Tokenize_unnumbered_list
-/// Tokenizes an unnumbered list
-pub fn tokenize_unnumbered_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}\n", tt);
-
-  let list_item = cs.get(0).unwrap();
-
-  println!("Tokenizing preceding whitespace...\n");
-
-  let ws = cs.get(1).unwrap();
-
-  lex.set_lexeme_limits(&ws);
-
-  // lex.update_pos();
-
-  lex.set_lexeme_limits(&list_item);
-
-  lex.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      list_item.start() + lex.pos.pos,
-      list_item.end() + lex.pos.pos
-    )
-  );
-
-  let bullet = cs.get(2).unwrap();
-
-  lex.set_lexeme_limits(&bullet);
-
-  lex.tokens.push(
-    Token::new(
-      TokenType::Bullet,
-      bullet.as_str().to_string(),
-      bullet.start() + lex.pos.pos,
-      bullet.end() + lex.pos.pos
-    )
-  );
-
-  // lex.update_pos();
-
-  lex.state = State::Inline;
+/// ### parse_unnumbered_list
+/// parses an unnumbered list
+pub fn parse_unnumbered_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
 }
 
-/// ### Tokenize_numbered_list
-/// Tokenizes an unnumbered list
-pub fn tokenize_numbered_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_numbered_list
+/// parses an unnumbered list
+pub fn parse_numbered_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### Tokenize_alpha_list
-/// Tokenizes an unnumbered list
-pub fn tokenize_alpha_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_alpha_list
+/// parses an unnumbered list
+pub fn parse_alpha_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
-/// ### Tokenize_definition_list
-/// Tokenizes an unnumbered list
-pub fn tokenize_definition_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_definition_list
+/// parses an unnumbered list
+pub fn parse_definition_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
-/// ### Tokenize_field_list
-/// Tokenizes an unnumbered list
-pub fn tokenize_field_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}\n", tt);
-
-}
-
-
-/// ### tokenize_literal_block
-/// Tokenizes a literal block
-pub fn tokenize_literal_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_field_list
+/// parses an unnumbered list
+pub fn parse_field_list(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_per_line_literal_block
-/// Tokenizes a per-line literal block
-pub fn tokenize_per_line_literal_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_literal_block
+/// parses a literal block
+pub fn parse_literal_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_line_block
-/// Tokenizes a line block
-pub fn tokenize_line_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_per_line_literal_block
+/// parses a per-line literal block
+pub fn parse_per_line_literal_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_paragraph
-/// Tokenizes a paragraph
-pub fn tokenize_paragraph(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_line_block
+/// parses a line block
+pub fn parse_line_block(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
-
-  let m = cs.get(0).unwrap();
-  let ws = cs.get(1).unwrap();
-  let par = cs.get(2).unwrap();
-
-  lex.set_lexeme_limits(&ws);
-
-  // lex.tokens.push(
-  //   Token::new(
-  //     TokenType::BlankLines,
-  //     String::from("\n\n"),
-  //     ws.start() + lex.pos.pos,
-  //     ws.end() + lex.pos.pos,
-  //   )
-  // );
-
-  // lex.update_pos();
-
-  lex.set_lexeme_limits(&par);
-
-  lex.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      par.start() + lex.pos.pos,
-      par.end() + lex.pos.pos,
-    )
-  );
-
-  lex.state = State::Inline;
 
 }
 
 
-/// ### tokenize_general_directive
-/// Tokenizes a paragraph
-pub fn tokenize_general_directive(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_paragraph
+/// parses a paragraph
+pub fn parse_paragraph(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+
+  println!("Tokenizing {:?}\n", tt);
+
+}
+
+
+/// ### parse_general_directive
+/// parses a paragraph
+pub fn parse_general_directive(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
@@ -220,36 +100,36 @@ pub fn tokenize_general_directive(lex: &mut Parser, tt:TokenType, cs: &regex::Ca
 
 
 
-/// ### tokenize_reference_target
-/// Tokenizes a reference target
-pub fn tokenize_reference_target(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_reference_target
+/// parses a reference target
+pub fn parse_reference_target(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_footnote_or_citation_target
-/// Tokenizes both footnote and citation targets
-pub fn tokenize_footnote_or_citation_target(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_footnote_or_citation_target
+/// parses both footnote and citation targets
+pub fn parse_footnote_or_citation_target(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_substitution_definition
-/// Tokenizes a subsititution definition target
-pub fn tokenize_substitution_definition(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_substitution_definition
+/// parses a subsititution definition target
+pub fn parse_substitution_definition(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
 }
 
 
-/// ### tokenize_comment
-/// Tokenizes a comment target
-pub fn tokenize_comment(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
+/// ### parse_comment
+/// parses a comment target
+pub fn parse_comment(lex: &mut Parser, tt:TokenType, cs: &regex::Captures) {
 
   println!("Tokenizing {:?}\n", tt);
 
--- src/parser/state_machine/transitions/inline.rs
+++ src/parser/state_machine/transitions/inline.rs
@@ -2,529 +2,81 @@
 use super::*;
 
 
-pub fn tokenize_escape (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_escape (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
   println!("\nTokenizing {:?}...", tt);
 
-  let s = cs.get(0).unwrap().start();
-  let e = cs.get(0).unwrap().end();
-  let c = cs.get(1).unwrap();
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(c.as_str()),
-      s + parser.pos.pos,
-      e + parser.pos.pos,
-    )
-  );
 }
 
-pub fn tokenize_code (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_code (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
   println!("\nTokenizing {:?}...", tt);
 
-  let m = cs.get(0).unwrap();
-  let code = cs.get(1).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(code.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
 }
 
 
-pub fn tokenize_inline_target_ref (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_inline_target_ref (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
   println!("\nTokenizing {:?}...", tt);
 
-  let m = cs.get(0).unwrap();
-  let link_alias = cs.get(1).unwrap();
-  let link = cs.get(2).unwrap();
-  let ref_type = cs.get(3).unwrap();
+}
 
-  parser.set_lexeme_limits(&m);
 
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos
-    )
-  );
+pub fn parse_inline_ref (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  parser.set_lexeme_limits(&link_alias);
 
-  parser.tokens.push(
-    Token::new(
-      TokenType::LinkAlias,
-      String::from(link_alias.as_str()),
-      link_alias.start() + parser.pos.pos,
-      link_alias.end() + parser.pos.pos
-    )
-  );
+}
 
-  parser.set_lexeme_limits(&link);
+pub fn parse_role_content (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  parser.tokens.push(
-    Token::new(
-      TokenType::Hyperlink,
-      String::from(link.as_str()),
-      link.start() + parser.pos.pos,
-      link.end() + parser.pos.pos
-    )
-  );
 
-  parser.set_lexeme_limits(&ref_type);
+}
 
-  parser.tokens.push(
-    Token::new(
-      TokenType::RefAnonOrNot,
-      String::from(ref_type.as_str()),
-      ref_type.start() + parser.pos.pos,
-      ref_type.end() + parser.pos.pos
-    )
-  );
-
-  parser.set_lexeme_limits(&m);
-
-  // parser.update_pos();
+pub fn parse_content_role (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
 }
 
 
-pub fn tokenize_inline_ref (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-  let target = cs.get(1).unwrap();
-  let ref_type = cs.get(2).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos
-    )
-  );
-
-  parser.set_lexeme_limits(&target);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::Target,
-      String::from(target.as_str()),
-      target.start() + parser.pos.pos,
-      target.end() + parser.pos.pos
-    )
-  );
-
-  parser.set_lexeme_limits(&ref_type);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::RefAnonOrNot,
-      String::from(ref_type.as_str()),
-      ref_type.start() + parser.pos.pos,
-      ref_type.end() + parser.pos.pos
-    )
-  );
-
-  parser.set_lexeme_limits(&m);
-
-  // parser.update_pos();
+pub fn parse_strong_emphasis (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
 }
 
-pub fn tokenize_role_content (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  println!("\nTokenizing {:?}...", tt);
+pub fn parse_emphasis (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  let m = cs.get(0).unwrap();
-  let role = cs.get(1).unwrap();
-  let content = cs.get(2).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  println!("\nTokenizing Role...");
-
-  parser.set_lexeme_limits(&role);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::Role,
-      String::from(role.as_str()),
-      role.start() + parser.pos.pos,
-      role.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-  println!("Tokenizing Content...");
-
-  parser.set_lexeme_limits(&content);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::Content,
-      String::from(content.as_str()),
-      content.start() + parser.pos.pos,
-      content.end() + parser.pos.pos,
-    )
-  );
-
-  parser.set_lexeme_limits(&m);
-
-  // parser.update_pos();
 
 }
 
-pub fn tokenize_content_role (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_footnote_or_citation (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  println!("\nTokenizing {:?}...", tt);
+}
 
-  let m = cs.get(0).unwrap();
-  let content = cs.get(1).unwrap();
-  let role = cs.get(2).unwrap();
 
-  parser.set_lexeme_limits(&m);
+pub fn parse_uri (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(""),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
+}
 
-  println!("\nTokenizing Content...");
-
-  parser.set_lexeme_limits(&content);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::Content,
-      String::from(role.as_str()),
-      content.start() + parser.pos.pos,
-      content.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-  println!("Tokenizing Role...");
-
-  parser.set_lexeme_limits(&role);
-
-  parser.tokens.push(
-    Token::new(
-      TokenType::Role,
-      String::from(content.as_str()),
-      role.start() + parser.pos.pos,
-      role.end() + parser.pos.pos,
-    )
-  );
-
-  parser.set_lexeme_limits(&m);
-
-  // parser.update_pos();
+pub fn parse_inline_whitespace (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
 
 }
 
 
-pub fn tokenize_strong_emphasis (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_text_no_ldelim (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  println!("\nTokenizing {:?}...", tt);
 
-  let m = cs.get(0).unwrap();
-  let text = cs.get(1).unwrap();
+}
 
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(text.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
+pub fn parse_text (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
 }
 
 
-pub fn tokenize_emphasis (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-  let text = cs.get(1).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(text.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
+pub fn parse_newline(parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
 }
 
-pub fn tokenize_footnote_or_citation (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
+pub fn parse_blankline(parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
 
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-  let target = cs.get(1).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(target.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-}
-
-
-pub fn tokenize_uri (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(m.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  if let Some(scheme) = cs.get(1) {
-    
-    parser.set_lexeme_limits(&scheme);
-
-    parser.tokens.push(
-      Token::new(
-        TokenType::Scheme,
-        String::from(scheme.as_str()),
-        scheme.start() + parser.pos.pos,
-        scheme.end() + parser.pos.pos
-      )
-    );
-
-  };
-
-  if let Some(authority) = cs.get(3) {
-    
-    parser.set_lexeme_limits(&authority);
-
-    parser.tokens.push(
-      Token::new(
-        TokenType::Authority,
-        String::from(authority.as_str()),
-        authority.start() + parser.pos.pos,
-        authority.end() + parser.pos.pos
-      )
-    );
-
-  };
-
-  if let Some(path) = cs.get(4) {
-    
-    parser.set_lexeme_limits(&path);
-
-    parser.tokens.push(
-      Token::new(
-        TokenType::Path,
-        String::from(path.as_str()),
-        path.start() + parser.pos.pos,
-        path.end() + parser.pos.pos
-      )
-    );
-
-  };
-
-  if let Some(query) = cs.get(6) {
-    
-    parser.set_lexeme_limits(&query);
-
-    parser.tokens.push(
-      Token::new(
-        TokenType::Query,
-        String::from(query.as_str()),
-        query.start() + parser.pos.pos,
-        query.end() + parser.pos.pos
-      )
-    );
-
-  };
-
-  if let Some(fragment) = cs.get(8) {
-    
-    parser.set_lexeme_limits(&fragment);
-
-    parser.tokens.push(
-      Token::new(
-        TokenType::Fragment,
-        String::from(fragment.as_str()),
-        fragment.start() + parser.pos.pos,
-        fragment.end() + parser.pos.pos
-      )
-    );
-
-  };
-
-  parser.set_lexeme_limits(&m);
-
-  // parser.update_pos();
-
-}
-
-pub fn tokenize_inline_whitespace (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-
-  parser.set_lexeme_limits(&m);
-  
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(" "),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-}
-
-
-pub fn tokenize_text_no_ldelim (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-
-  let m = cs.get(0).unwrap();
-
-  parser.set_lexeme_limits(&m);
-  
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(m.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-}
-
-pub fn tokenize_text (parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("\nTokenizing {:?}...", tt);
-  
-  let m = cs.get(0).unwrap();
-  
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(m.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-}
-
-
-pub fn tokenize_newline(parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}...\n", tt);
-
-  let m = cs.get(0).unwrap();
-
-  parser.set_lexeme_limits(&m);
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from(m.as_str()),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-}
-
-pub fn tokenize_blankline(parser: &mut Parser, tt: TokenType, cs: &regex::Captures) {
-
-  println!("Tokenizing {:?}...\n", tt);
-
-  let m = cs.get(0).unwrap();
-
-  parser.tokens.push(
-    Token::new(
-      tt,
-      String::from("\n\n"),
-      m.start() + parser.pos.pos,
-      m.end() + parser.pos.pos,
-    )
-  );
-
-  // parser.update_pos();
-
-  parser.state = State::Body
 }
 
