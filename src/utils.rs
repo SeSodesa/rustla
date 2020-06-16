6b86df3d9b0642658d56c2f6d6afa502fb759ecc
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-06-16 12:01:51 +0300

    Added function utils::strip_indent

--- src/utils.rs
+++ src/utils.rs
@@ -26,3 +26,32 @@ where P: AsRef<path::Path> {
   let file:fs::File = fs::File::open(file_path)?;
   Ok(io::BufReader::new(file).lines())
 }
+
+
+/// ### strip_indent
+/// A whitespace-aware function for stripping indentation
+/// from `String`s. Returns `Ok(String)` if successful.
+/// If non-whitespace characters are encountered before
+/// the notified `amount` has been stripped, an `Err(message)`
+/// is returned instead.
+pub fn strip_indent(line: String, amount: usize) -> Result<String, &'static str> {
+
+  if line.is_empty() {
+    return Ok(line)
+  }
+
+  let mut chars = line.chars();
+
+  for i in 0..amount {
+
+    let c = chars.next().unwrap();
+
+    if !c.is_whitespace() && i < amount {
+      return Err("Non-whitespace character encountered before supposed indentation level reached.\n");
+    }
+
+  }
+
+  Ok(chars.as_str().to_string())
+
+}
