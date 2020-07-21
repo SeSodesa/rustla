bf1ff4c6dc0c307d46e9936d36766d807ab01652
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-07-21 14:28:31 +0300

    Added some documentation to TreeNodeTypes via comments, and removed a few node types that will most likely go unused in the future.

--- src/doctree/mod.rs
+++ src/doctree/mod.rs
@@ -158,31 +158,62 @@ impl TreeNode {
 #[derive(Debug, PartialEq)]
 pub enum TreeNodeType {
 
-  // DocTree root node
+  /// #### Root
+  /// The root node of an reStructuredText document tree.
+  /// Contains the (name|absolute path) of the document
+  /// as its only field
   Root{
     doc_name: String
   },
 
-  // Simple empty line with no additional data
+  /// #### EmptyLine
+  /// A simple empty line, that contains no actual data.
+  /// These can be contained in pretty much any container
+  /// node, such as lists or list items, in addition to
+  /// existing between body level elements.
   EmptyLine,
 
-  // Structural elements
-  Section(structural_nodes::Section),
-  Topic(structural_nodes::Topic),
-  Transition(structural_nodes::Transition),
+  /// #### Section
+  /// A section title node, that contains the title text,
+  /// in addition to its marker type and (sub)section level.
+  Section {
+
+  },
+
+  /// #### Transition
+  /// A node corresponding to LaTeX's `\hrulefill` command.
+  Transition {
+
+  },
 
   // Body level elements
+
+  /// #### Paragraph
+  /// A node constructed of a left-aligned block of text
+  /// with no special starter markers.
   Paragraph,
+
+  /// #### BulletList
+  /// An unnumbered list node. These may only contain `BulletListItem` nodes
+  /// or `EmptyLine`s as their direct children.
   BulletList {
     bullet: char,
     bullet_indent: usize,
     text_indent: usize
   },
+
+  /// #### BulletListItem
+  /// An unnumbered list item. Cna contain any `Body` level elements
+  /// as its direct children.
   BulletListItem{
     bullet: char,
     bullet_indent: usize,
     text_indent: usize
   },
+
+  /// #### EnumeratedList
+  /// An enumerated list node. Cna only contain `EnumeratedListItem` and `EmptyLine`
+  /// nodes as its direct children.
   EnumeratedList {
     delims: EnumDelims,
     kind: EnumKind,
@@ -191,6 +222,10 @@ pub enum TreeNodeType {
     enumerator_indent: usize,
     latest_text_indent: usize
   },
+
+  /// #### EnumeratedListItem
+  /// Child node type of `EnumeratedList`. Can contain any `Body`elements
+  /// as its children.
   EnumeratedListItem {
     delims: EnumDelims,
     kind: EnumKind,
@@ -198,112 +233,243 @@ pub enum TreeNodeType {
     enumerator_indent: usize,
     text_indent: usize
   },
+
+  /// #### DefinitionList
+  /// A list of definitions. Contains `DefinitionListItems` or `EmptyLine` nodes
+  /// as its direct children.
   DefinitionList(body_nodes::DefinitionList),
+
+  /// #### DefinitionListItem
+  /// A child node type of `DefinitionList`.
+  /// Contains a map of `DefinitionTerm`s and the corresponding
+  /// `TermDefinitions`, in addition to optional term classifiers.
   DefinitionListItem(body_nodes::DefinitionListItem),
-  // Term(body_nodes::Term),
-  // Classifier(body_nodes::Classifier),
-  Definition(body_nodes::Definition),
-  FieldList(body_nodes::FieldList),
-  Field(body_nodes::Field),
-  FieldName(body_nodes::FieldName),
-  FieldBody(body_nodes::FieldBody),
-  Option(body_nodes::Option),
-  OptionArgument(body_nodes::OptionArgument),
-  OptionGroup(body_nodes::OptionGroup),
-  OptionList(body_nodes::OptionList),
-  OptionListItem(body_nodes::OptionListItem),
-  OptionString(body_nodes::OptionString),
-  Description(body_nodes::Description),
-  LiteralBlock(body_nodes::LiteralBlock),
-  DoctestBlock(body_nodes::DoctestBlock),
-  MathBlock(body_nodes::MathBlock),
-  LineBlock(body_nodes::LineBlock),
-  Line(body_nodes::Line),
-  BlockQuote(body_nodes::BlockQuote),
-  Attribution(body_nodes::Attribution),
-  Attention(body_nodes::Attention),
-  Caution(body_nodes::Caution),
-  Danger(body_nodes::Danger),
-  Error(body_nodes::Error),
-  Important(body_nodes::Important),
-  Note(body_nodes::Note),
-  Tip(body_nodes::Tip),
-  Hint(body_nodes::Hint),
-  Warning(body_nodes::Warning),
-  Admonition(body_nodes::Admonition),
-  Comment(body_nodes::Comment),
-  SubstitutionDefinition(body_nodes::SubstitutionDefinition),
-  Target(body_nodes::Target),
-  Footnote(body_nodes::Footnote),
-  Citation(body_nodes::Citation),
-  Label(body_nodes::Label),
-  Figure(body_nodes::Figure),
-  Caption(body_nodes::Caption),
-  Legend(body_nodes::Legend),
-  Table(body_nodes::Table),
-  TableGroup(body_nodes::TableGroup),
-  ColSpec(body_nodes::ColSpec),
-  TableHead(body_nodes::TableHead),
-  TableBody(body_nodes::TableBody),
-  TableRow(body_nodes::TableRow),
-  TableEntry(body_nodes::TableEntry),
+
+  /// #### DefinitionTerm
+  /// The term that is to be defined in a `DefinitionList`.
+  DefinitionTerm(body_nodes::Term),
+
+  /// #### Classifier
+  /// A classifier for a `DefinitionTerm` in a `DefinitionList`.
+  /// Could be the type of a varible in a function decraration, or something similar.
+  Classifier(body_nodes::Classifier),
+
+  /// #### TermDefinition
+  /// A definition of a term in a `DefinitionList`.
+  TermDefinition(body_nodes::Definition),
+
+  /// #### FieldList
+  /// A list of fields, that are used as a part of the
+  /// reStructuredText extension syntax, such as directives.
+  FieldList {
+
+  },
+
+  /// #### FieldListItem
+  /// A field item of a `FieldList`.
+  FieldListItem,
+
+  /// #### FieldName
+  /// They key to the field body.
+  FieldName,
+
+  /// #### FieldBody
+  /// The parameter that `FieldName` refers to.
+  FieldBody,
+  
+  /// #### OptionList
+  /// A two-column list of command line options, such as the ones typically seen on unix `man` pages.
+  /// Four types of options are supported:
+  ///
+  /// 1. short POSIX options with one '-' and an opion letter,
+  /// 2. Long POSIX options with "--", followed by an option word.
+  ///    Some systems might use a single dash.
+  /// 3. Old GNU-style options starting with a '+', followed by an option letter (!!!deprecated!!!)
+  /// 4. DOS/VMS options starting with a '/', followed by an option letter or a word.
+  /// 
+  /// The recognized syntax is based on Python's `getopt.py` module.
+  OptionList,
+
+  /// #### OptionListItem
+  /// A single option in an `OptionList`. Consists of an option,
+  /// folllowed by and optional argument and a description.
+  /// May contain arbitrary indented body elements after these:
+  /// ```text
+  /// +----------------------------+-------------+
+  /// | option [" " argument] "  " | description |
+  /// +-------+--------------------+             |
+  ///         | (body elements)+                 |
+  ///         +----------------------------------+
+  /// ```
+  OptionListItem,
+
+  /// #### LiteralBlock
+  /// Paragraph (possibly empty) ending in a "::" signifies the start of a literal block of text.
+  /// Text contained in a literal block is not interpreted in any way,
+  /// but simply stored in this node as is.
+  LiteralBlock,
+
+  /// #### DoctestBlock
+  /// These are interactive Python sessions contained in Python docstrings.
+  /// Based on the Python standard library [doctest](http://www.python.org/doc/current/lib/module-doctest.html) module.
+  /// 
+  /// Doctest blocks begin with ">>>", the python REPL main prompt and end with a blank line.
+  /// They are a special case of the literal block and if both are present,
+  /// the literal block takes precedence.
+  DoctestBlock,
+  
+  /// #### MathBlock
+  /// A node for display-style mathematics (LaTeX).
+  MathBlock,
+
+  /// #### LineBlock
+  /// A block of text where each new line begins with an unindented '|',
+  /// followed be text with specific left-alignment, used as a reference
+  /// for the rest of the block.
+  /// Allows writing blocks of text, where the struture of the lines
+  /// is meaningful, such as poetry.
+  /// 
+  /// The symbols '|' may be omitted, as they signify the start of a new
+  /// line in the rendered output.
+  /// ```txt
+  /// +------+-----------------------+
+  /// | "| " | line                  |
+  /// +------| continuation line     |
+  ///        +-----------------------+
+  /// ```
+  LineBlock,
+
+  /// #### Line
+  /// A general line node. Might signify the start of a transtition or a section title.
+  Line,
+  
+  /// #### BlockQuote
+  /// Text indented relative to previous text,
+  /// without markup indicating the start of
+  /// a list or other container nodes.
+  /// A block quote may end with an `Attribution`,
+  /// which allows placing multiple block quotes
+  /// in a sequence.
+  BlockQuote,
+
+  /// #### Attribution
+  /// An optional attribution of a `BlockQuote`.
+  /// If a `BlockQuote` contains an attribution,
+  /// the following node may be a `BlockQuote as well,
+  /// but not otherwise.
+  Attribution,
+
+  /// #### SubstitutionDefinition
+  /// Explicit markup node, as in begins with ".. " followed by a vertical bar '|',
+  /// substitution text and another '|'. The text may not begin or end with whitespace.
+  /// Substitution definition blocks may contain a nested, *inline compatible* directive
+  /// *without* the leading ".. ", such as `image` or `replace`.
+  /// ```text
+  /// +-------+-----------------------------------------------------+
+  /// | ".. " | "|" substitution text "| " directive type "::" data |
+  /// +-------+ directive block                                     |
+  ///         |                                                     |
+  ///         +-----------------------------------------------------+
+  /// ```
+  SubstitutionDefinition,
+
+  /// #### Footnote
+  /// A foonote citation target. Contaisn a label and the foornote text itself.
+  Footnote,
+
+  /// #### Citation
+  /// A generic citation target.
+  Citation,
+
 
   // Inline elements
+  // ---------------
+
+  /// #### Text
+  /// A plain text node, that contains no special markup.
   Text {
     text:String
   },
+
+  /// #### Emphasis
+  /// Emphasised or italicized text.
   Emphasis {
     text: String
   },
+
+  /// #### StrongEmphasis
+  /// Strongly emphasised text, usually rendered in bold.
   StrongEmphasis {
     text:String
   },
+
+  /// #### Literal
+  /// Literal text, usually reserved for code.
   Literal {
     text: String
   },
+
+  /// #### InlineTarget
+  /// An inline reference target.
   InlineTarget {
     target_label: String
   },
+
+  /// #### Reference
+  /// A general reference to a reference target.
   Reference {
     target_label: String
   },
+
+  /// #### FootnoteReference
+  /// A reference to a foot note.
   FootnoteReference {
     target_label: String
   },
+
+  /// #### CitationReference
+  /// A reference to a bibliographic citation.
   CitationReference {
     target_label: String
   },
+
+  /// #### SubstitutionReference
+  /// A reference that is to be substituted with the reference target directive output.
   SubstitutionReference {
     target_label: String
   },
-  TitleReference(inline_nodes::TitleReference),
-  AbsoluteURI{text: String},
-  StandaloneEmail{text: String},
-  Abbreviation(inline_nodes::Abbreviation),
-  Acronym(inline_nodes::Acronym),
-  SuperScript(inline_nodes::SuperScript),
-  SubScript(inline_nodes::SubScript),
-  Math(inline_nodes::Math),
-  Image(inline_nodes::Image),
-  Inline(inline_nodes::Inline),
-  Problematic(inline_nodes::Problematic),
-  Generated(inline_nodes::Generated),
-  WhiteSpace{text: String},
+
+  /// #### TitleReference
+  /// A reference to a title.
+  TitleReference {
+    target_label: String
+  },
+
+  /// ### AbsoluteURI
+  /// A reference to a web address.
+  AbsoluteURI{
+    text: String
+  },
+
+  /// #### StandaloneEmail
+  /// A reference to an email address.
+  StandaloneEmail{
+    text: String
+  },
+
+  /// #### Math
+  /// An inline math node.
+  Math {
+    text: String
+  },
+
+  /// #### Whitespace
+  /// Generic whitespace that covers everything from spaces to newlines.
+  WhiteSpace{
+    text: String
+  },
 
 }
-
-// #[derive(Debug, PartialEq)]
-// /// ### EnumeratorType
-// /// An enumeration of the different enumerator types
-// /// that an enumerated list might have. Used for comparing
-// /// enumerated list items on the same indentation (nesting)
-// /// level. A list item needs to have the same enumerator type
-// /// as the containing enumerated list.
-// pub enum EnumeratorType {
-//   ArabicParens, LowerAlphaParens, UpperAlphaParens, LowerRomanParens, UpperRomanParens,
-//   ArabicRParen, LowerAlphaRParen, UpperAlphaRParen, LowerRomanRParen, UpperRomanRParen,
-//   ArabicPeriod, LowerAlphaPeriod, UpperAlphaPeriod, LowerRomanPeriod, UpperRomanPeriod,
-// }
 
 
 /// ### Parent
