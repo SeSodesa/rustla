09ac3aab11d0d2db4ca39a3117e93f494f7672c3
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-07-22 14:14:06 +0300

    Started work on parser::state_machine::field_list::field_marker...

--- src/parser/state_machine/field_list.rs
+++ src/parser/state_machine/field_list.rs
@@ -1,11 +1,81 @@
 /// ## field_list
-/// A submodule that contains transition functions responsible for creating FieldList nodes.
+/// A submodule that contains transition functions responsible for creating FieldListItem nodes.
 
 use super::*;
 
 
 /// ### field_marker
-/// Creates FieldListItems, if parameters such as detected indentation and such match with the parent node paraneters.
+/// Creates FieldListItems, if parameters such as detected indentation and such match with the parent node ones.
 pub fn field_marker (src_lines: &Vec<String>, base_indent: &usize, current_line: &mut usize, doctree: Option<DocTree>, captures: regex::Captures, pattern_name: &PatternName) -> TransitionResult {
-  todo!()
+
+  let mut tree_wrapper = doctree.unwrap();
+
+  let detected_text_indent = captures.get(0).unwrap().as_str().chars().count() + base_indent;
+  let detected_marker_indent = captures.get(1).unwrap().as_str().chars().count() + base_indent;
+  let detected_marker_name = captures.get(2).unwrap().as_str();
+
+  let detected_body_indent = if let Some(next_line) = src_lines.get(*current_line + 1) {
+    next_line.chars().take_while(|c| c.is_whitespace()).count() + base_indent
+  } else {
+    detected_text_indent
+  };
+
+  // Make sure we are inside a FieldList and that indentations match
+  match tree_wrapper.tree.node.data {
+
+    TreeNodeType::FieldList { marker_indent} => {
+      if marker_indent == detected_marker_indent {
+        let marker_inline_nodes = if let Some(nodes) = Parser::inline_parse(detected_marker_name.to_string(), current_line) {
+          nodes
+        } else {
+          return TransitionResult::Failure { // Should not happen in the first place, if a field marker was detected...
+            message: format!("Tried parsing a field marker on line {} for inline nodes but none found.\nMarker not valid...\n", current_line)
+          }
+        };
+        let item_node_data = TreeNodeType::FieldListItem {
+          raw_marker_name: detected_marker_name.to_string(),
+          marker_name_as_inline_nodes: marker_inline_nodes,
+          marker_indent: detected_marker_indent,
+          body_indent: detected_body_indent
+        };
+
+        tree_wrapper.tree = tree_wrapper.tree.push_and_focus(item_node_data).unwrap();
+
+        // Read first block of list item here with a nested parser and add generated nodes to tree...
+
+        return TransitionResult::Success {
+          doctree: tree_wrapper,
+          next_state: Some(StateMachine::ListItem),
+          push_or_pop: PushOrPop::Push,
+          line_advance: LineAdvance::None,
+          nested_state_stack: None
+        }
+
+      } else {
+        tree_wrapper.tree = tree_wrapper.tree.focus_on_parent().unwrap();
+        return TransitionResult::Success {
+          doctree: tree_wrapper,
+          next_state: None,
+          push_or_pop: PushOrPop::Pop,
+          line_advance: LineAdvance::None,
+          nested_state_stack: None
+        }
+      }
+    }
+
+    _ => {} // Not inside a field list...
+
+  }
+
+  return TransitionResult::Failure {
+    message: format!("Attempted parsing a FieldListItem outside a FieldList on line {}.\nComputer says no...\n", current_line)
+  }
+
+}
+
+
+fn parse_raw_marker_str_for_inline_nodes (marker_str: &str, current_line: &mut usize) -> Option<Vec<TreeNode>> {
+  
+  Parser::inline_parse(marker_str.to_string(), current_line)
+  
 }
