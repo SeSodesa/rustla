8614baaa056f2b381137fdb644830f9a82f89530
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-08-12 12:16:02 +0300

    Added skeletons for directive parsing functions under parser

--- src/parser/directive_parsers.rs
+++ src/parser/directive_parsers.rs
@@ -9,5 +9,258 @@
 use super::*;
 
 impl Parser {
-  
+
+  pub fn parse_standard_admonition () {
+    todo!()
+  }
+
+
+  pub fn parse_generic_admonition () {
+    todo!()
+  }
+
+
+  pub fn parse_image () {
+    todo!()
+  }
+
+
+  pub fn parse_figure () {
+    todo!()
+  }
+
+
+  pub fn parse_topic () {
+    todo!()
+  }
+
+
+  pub fn parse_sidebar () {
+    todo!()
+  }
+
+
+  pub fn parse_line_block () {
+    todo!()
+  }
+
+
+  pub fn parse_parsed_literal () {
+    todo!()
+  }
+
+
+  pub fn parse_code () {
+    todo!()
+  }
+
+
+  pub fn parse_math () {
+    todo!()
+  }
+
+
+  pub fn parse_rubric () {
+    todo!()
+  }
+
+
+  pub fn parse_epigraph () {
+    todo!()
+  }
+
+
+  pub fn parse_highlights () {
+    todo!()
+  }
+
+
+  pub fn parse_pull_quote () {
+    todo!()
+  }
+
+
+  pub fn parse_compound () {
+    todo!()
+  }
+
+
+  pub fn parse_container () {
+
+  }
+
+
+  pub fn parse_table () {
+    todo!()
+  }
+
+
+  pub fn parse_csv_table () {
+    todo!()
+  }
+
+
+  pub fn parse_list_table () {
+    todo!()
+  }
+
+
+  pub fn parse_contents () {
+    todo!()
+  }
+
+
+  pub fn parse_section_numbering () {
+    todo!()
+  }
+
+
+  pub fn parse_header_or_footer () {
+    todo!()
+  }
+
+
+  pub fn parse_target_notes () {
+    todo!()
+  }
+
+
+  pub fn parse_footnotes () {
+    todo!()
+  }
+
+
+  pub fn parse_citations () {
+    todo!()
+  }
+
+
+  pub  fn parse_meta () {
+    todo!()
+  }
+
+
+  pub fn parse_include () {
+    todo!()
+  }
+
+
+  pub fn parse_raw () {
+    todo!()
+  }
+
+
+  pub fn parse_class () {
+    todo!()
+  }
+
+
+  pub fn parse_role () {
+    todo!()
+  }
+
+
+  pub fn parse_default_role () {
+    todo!()
+  }
+
+
+  pub fn parse_title () {
+    todo!()
+  }
+
+
+  pub fn restucturetext_test_directive () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_questionnaire () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_submit () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_toctree () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_active_element_input () {
+    todo!()
+  }
+
+
+  pub fn parse_apkus_active_element_output () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_hidden_block () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_point_of_interest () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_annotated () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_lineref_codeblock () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_repl_res_count_reset () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_acos_submit () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_div () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_styled_topic () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_story () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_jsvee () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_youtube () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_local_video () {
+    todo!()
+  }
+
+
+  pub fn parse_aplus_embedded_page () {
+    todo!()
+  }
 }
