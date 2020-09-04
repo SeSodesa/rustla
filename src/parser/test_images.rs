3eace78954cf6312623dd269106f8403fb5fd45f
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-09-04 10:34:01 +0300

    Figures inside of figures seem to work

--- src/parser/test_images.rs
+++ src/parser/test_images.rs
@@ -214,3 +214,46 @@ Back to no indentation.
     _ => panic!()
   }
 }
+
+
+#[test]
+fn figure_03 () {
+  let src = String::from("
+.. figure:: this/is/an/image/uri.png
+
+  .. figure:: this/is/another/image/uri.png
+
+    Caption paragraph: A figure inside a figure legend.
+
+Back to no indentation.
+
+  ").lines().map(|s| s.to_string()).collect::<Vec<String>>();
+
+  let mut doctree = DocTree::new(String::from("test"));
+
+  let mut parser = Parser::new(src, doctree, None, 0, None, 0);
+
+  doctree = parser.parse().unwrap_tree();
+  doctree = doctree.walk_to_root();
+  doctree.print_tree();
+
+  match doctree.shared_child(1).shared_data() {
+    TreeNodeType::Figure { .. } => {}
+    _ => panic!()
+  }
+
+  match doctree.shared_child(1).shared_child(2).shared_data() {
+    TreeNodeType::Figure { .. } => {}
+    _ => panic!()
+  }
+
+  match doctree.shared_child(1).shared_child(2).shared_child(2).shared_data() {
+    TreeNodeType::Paragraph { .. } => {}
+    _ => panic!()
+  }
+
+  match doctree.shared_child(2).shared_data() {
+    TreeNodeType::Paragraph { .. } => {}
+    _ => panic!()
+  }
+}
