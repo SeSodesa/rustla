f3e2fbe711d33c636453973a02ea74842cab0053
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-09-15 11:04:57 +0300

    Started work on another interpreted text test

--- src/parser/test_inline_parsing.rs
+++ src/parser/test_inline_parsing.rs
@@ -388,3 +388,26 @@ The following should produce a title reference: `Söderholm2020`.
     panic!()
   }
 }
+
+
+#[test]
+fn interpreted_text_02 () {
+
+  let src = String::from(r#"
+Interpreted text with two roles produces a literal: :emphasis:`text here`:strong:.
+
+Unknown roles also trigger literal text: :role1:`this is a literal`,
+`as is this`:role2:.
+
+"#).lines().map(|s| s.to_string()).collect::<Vec<String>>();
+
+  let mut doctree = DocTree::new(PathBuf::from("test"));
+
+  let mut parser = Parser::new(src, doctree, None, 0, None, 0);
+
+  doctree = parser.parse().unwrap_tree();
+  doctree = doctree.walk_to_root();
+  doctree.print_tree();
+
+  todo!()
+}
