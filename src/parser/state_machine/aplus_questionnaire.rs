347ee505d53b126c7f5b1971b22d9ff09cd43714
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-09-22 12:49:08 +0300

    Impelemented the parsing of intermediate text blocks inside A+ questionnaires

--- src/parser/state_machine/aplus_questionnaire.rs
+++ src/parser/state_machine/aplus_questionnaire.rs
@@ -12,7 +12,53 @@ use super::*;
 ///
 /// A function for reading in blocks of intermediate text (rST paragraphs) between questionnaire questions.
 pub fn aplus_questionnaire_text (src_lines: &Vec<String>, base_indent: usize, section_level: &mut usize, line_cursor: &mut LineCursor, doctree: Option<DocTree>, captures: regex::Captures, pattern_name: &PatternName) -> TransitionResult  {
-  todo!()
+
+  let mut doctree = doctree.unwrap();
+
+  let detected_indent = captures.get(1).unwrap().as_str().chars().count();
+
+  match Parser::parent_indent_matches(doctree.shared_data(), detected_indent) {
+    IndentationMatch::JustRight => {
+
+      let start_line = line_cursor.relative_offset();
+      let indent_allowed = true;
+      let remove_indent = true;
+      let alignment = Some(detected_indent);
+      let (block_lines, offset) = if let Ok((lines, offset)) = Parser::read_text_block(src_lines, start_line, indent_allowed, remove_indent, alignment) {
+        (lines, offset)
+      } else {
+        panic!("Error when reading intermediate text in A+ questionnaire on line {}. Computer says no...", line_cursor.sum_total())
+      };
+
+      let mut inline_nodes = match Parser::inline_parse(block_lines.join("\n"), None, line_cursor) {
+        InlineParsingResult::Nodes(nodes) => nodes,
+        _ => panic!("Cound not parse intermediate questionnaire text on line {} for inline nodes. Computer says no...", line_cursor.sum_total())
+      };
+
+      let paragraph = TreeNodeType::Paragraph { indent: detected_indent };
+      doctree = doctree.push_data_and_focus(paragraph);
+      for node in inline_nodes {
+        doctree = doctree.push_data(node);
+
+      }
+      doctree = doctree.focus_on_parent();
+      return TransitionResult::Success {
+        doctree: doctree,
+        next_states: None,
+        push_or_pop: PushOrPop::Neither,
+        line_advance: LineAdvance::Some(1)
+      };
+    }
+    _ => {
+      doctree = doctree.focus_on_parent();
+      return TransitionResult::Success {
+        doctree: doctree,
+        next_states: None,
+        push_or_pop: PushOrPop::Pop,
+        line_advance: LineAdvance::None,
+      }
+    }
+  }
 }
 
 /// ### aplus_pick_one
