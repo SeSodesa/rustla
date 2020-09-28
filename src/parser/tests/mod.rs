1d118342c1bd1aebac50ee7b0eefc47f4ebecfb1
Santtu Söderholm <santtu.soderholm@hotmail.com>
2020-09-28 13:56:17 +0300

    Moved parser tests to their own folder under src/parser/tests

---
+++ src/parser/tests/mod.rs
@@ -0,0 +1,29 @@
+/// ## tests
+///
+/// A submodule for Parser-related tests.
+///
+/// author: Santtu Söderholm
+/// email:  santtu.soderholm@tuni.fi
+
+use super::*;
+
+mod test_admonitions;
+mod test_aplus_point_of_interest;
+mod test_aplus_questionnaire;
+mod test_block_reading;
+mod test_block_quotes;
+mod test_comments;
+mod test_converters;
+mod test_bullet_lists;
+mod test_definition_lists;
+mod test_enumerated_lists;
+mod test_field_lists;
+mod test_hyperlink_targets;
+mod test_images;
+mod test_math_blocks;
+mod test_mixed_structures;
+mod test_inline_parsing;
+mod test_literal_blocks;
+mod test_sections_and_transitions;
+mod test_sphinx_only;
+mod test_unknown_directives;
