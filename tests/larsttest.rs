089e2ac15bf836f873aaa3f033ceb59b365233d9
Santtu Söderholm <santtu.soderholm@hotmail.com>
2021-01-08 21:07:20 +0200

    Added larst version-20-dev output strings for the test files in the larsttest repository
    https://course-gitlab.tuni.fi/ITC/CS/larst/larsttest

--- tests/larsttest.rs
+++ tests/larsttest.rs
@@ -40,19 +40,185 @@ id est laborum.
 
 #[test]
 fn enumerate () {
+    let src = common::str_to_lines(
+"
+Something before
+
+#. First :math:`X=1`
+    with wrapping
+#. Next :math:`X=2` and something
+    that continues to the next line!
+
+    * Recursive items
+    * Another recursive
+        item continues
+        to the next line
+        how deep
+        this could go
+        after
+        all
+
+#. An this list just
+    goes on!
+
+Something after
+
+#. .. image:: fig/clique.*
+
+#. .. image:: fig/clique.*
+
+"
+            );
+            let mut doctree = DocTree::new(PathBuf::new());
+            doctree = Parser::new(src, doctree, Some(0), 0, Some(State::Body), 0)
+                .parse()
+                .unwrap_tree();
+            doctree = doctree.walk_to_root();
+            doctree.print_tree();
     todo!()
 }
+
 #[test]
 fn itemize () {
+    let src = common::str_to_lines(
+"
+Something before
+
+* First :math:`X=1`
+  with wrapping
+* Next :math:`X=2` and something
+  that continues to the next line!
+
+  #. Recursive items
+  #. Another recursive
+     item continues
+     to the next line
+     how deep
+     this could go
+     after
+     all
+
+* An this list just
+  goes on!
+
+Something after
+
+* .. image:: fig/clique.*
+
+* .. image:: fig/clique.*
+
+
+"
+    );
+    let mut doctree = DocTree::new(PathBuf::new());
+    doctree = Parser::new(src, doctree, Some(0), 0, Some(State::Body), 0)
+        .parse()
+        .unwrap_tree();
+    doctree = doctree.walk_to_root();
+    doctree.print_tree();
     todo!()
 }
 
 #[test]
 fn tabular () {
+    let src = common::str_to_lines(
+"
+Tähän vähän tekstiä
+
+ja sitten vähän lisää.
+
+.. list-table:: ABCD
+
+  * - A
+    - B
+  * - C
+    - D
+
+Taulukko 1: Elementit A, B, C ja D
+
+.. list-table::
+
+  * - .. image:: fig/clique.*
+        :align: center
+
+    - .. image:: fig/clique.*
+
+.. list-table::
+
+  * - The variables are as follows:
+
+      * A
+      * B
+      * C
+
+    - .. image:: fig/clique.*
+
+Taulukon jälkeenkin jotain.
+
+Jatkoa seuraa.
+
+.. list-table::
+  :widths: 67 33
+
+  * - The variables are as follows:
+
+      * A
+      * B
+
+        .. math::
+
+          B=C
+
+      * C
+
+    - .. image:: fig/clique.*
+
+One
+Two
+Three
+
+"
+    );
+    let mut doctree = DocTree::new(PathBuf::new());
+    doctree = Parser::new(src, doctree, Some(0), 0, Some(State::Body), 0)
+        .parse()
+        .unwrap_tree();
+    doctree = doctree.walk_to_root();
+    doctree.print_tree();
     todo!()
 }
 
 #[test]
 fn thebibliography () {
+    let src = common::str_to_lines(
+r#"
+.. [DG84]
+
+  W.\ Dowling and J.\ Gallier.
+  Linear time algorithms for testing the satisfiability of
+  propositional Horn formulae.
+  *Journal of Logic Programming*, 1(3):267--284, 1984.
+
+.. [Lloyd87]
+
+  J.\ Lloyd.
+  *Foundations of Logic Programming*.
+  Springer, 1987.
+
+.. [MMZZM01]
+
+  M.\ Moskewicz, C.\ Madigan, Y.\ Zhao, L.\ Zhang, and S.\ Malik.
+  Chaff: Engineering an efficient SAT solver.
+  In *Proceedings of DAC 2001*, pages 530--5350, 2001.
+  URL:
+  https://www.princeton.edu/~chaff/publication/DAC2001v56.pdf.
+"#
+    );
+    let mut doctree = DocTree::new(PathBuf::new());
+    doctree = Parser::new(src, doctree, Some(0), 0, Some(State::Body), 0)
+        .parse()
+        .unwrap_tree();
+    doctree = doctree.walk_to_root();
+    doctree.print_tree();
     todo!()
 }
